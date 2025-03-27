7476831fe4cfc8e2e9deee05288f18ecf201c6c5
David Clarke <david_anthony_clarke@hotmail.com>
2025-03-27 13:39:02 +1100

    Update analysis.R

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -2,6 +2,9 @@
 ## Analyses
 ################################################################################
 
-## Load required libraries
+## Ensure necessary packages (and versions) are installed
 renv::restore()
 
+## Load required libraries
+pkgs <- c("here", "sf", "terra", "magrittr", "dplyr")
+lapply(pkgs, require, character.only = T)
